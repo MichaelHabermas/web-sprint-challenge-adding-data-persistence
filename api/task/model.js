9c8a3053247c5e38638cda02b5de9723315feab5
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-11 13:20:41 -0400

    cleanup

--- api/task/model.js
+++ api/task/model.js
@@ -14,13 +14,21 @@ async function getAllTasks() {
 		.orderBy('task_id');
 
 	return tasks.map(task => {
+		const {
+			task_id,
+			task_description,
+			task_notes,
+			task_completed,
+			project_name,
+			project_description
+		} = task;
 		return {
-			task_id: task.task_id,
-			task_description: task.task_description,
-			task_notes: task.task_notes,
-			task_completed: task.task_completed ? true : false,
-			project_name: task.project_name,
-			project_description: task.project_description
+			task_id: task_id,
+			task_description: task_description,
+			task_notes: task_notes,
+			task_completed: task_completed ? true : false,
+			project_name: project_name,
+			project_description: project_description
 		};
 	});
 }
