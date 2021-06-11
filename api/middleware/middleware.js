91216d5b9d264b641f8ac0af92654318d787ab7a
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-11 11:09:21 -0400

    gets working

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,6 +1,7 @@
 const Task = require('../task/model');
 const Project = require('../project/model');
 const Resource = require('../resource/model');
+const db = require('../../data/dbConfig');
 
 function logger(req, res, next) {
 	console.log(`[${new Date().toLocaleString()}] [${req.method}] ${req.path}`);
@@ -8,18 +9,44 @@ function logger(req, res, next) {
 }
 
 function validateProject(req, res, next) {
-	console.log('validateProject Working');
-	next();
+	const { project_name } = req.body;
+	if (!project_name || typeof project_name !== 'string') {
+		next({
+			status: 400,
+			message: 'Invalid project name'
+		});
+	} else {
+		next();
+	}
 }
 
 function validateResource(req, res, next) {
-	console.log('validateResource Working');
-	next();
+	const { resource_name } = req.body;
+	if (!resource_name || typeof resource_name !== 'string') {
+		next({
+			status: 400,
+			message: 'Invalid resource name'
+		});
+	} else if (Resource.isResourceNameUnique(resource_name)) {
+		next({
+			status: 400,
+			message: 'Resource name must be unique'
+		});
+	} else {
+		next();
+	}
 }
 
 function validateTask(req, res, next) {
-	console.log('validateTask Working');
-	next();
+	const { task_description, project_id } = req.body;
+	if (!task_description || typeof task_description !== 'string') {
+		next({
+			status: 400,
+			message: 'Invalid project name'
+		});
+	} else {
+		next();
+	}
 }
 
 module.exports = { logger, validateResource, validateProject, validateTask };
--- api/resource/model.js
+++ api/resource/model.js
@@ -8,4 +8,11 @@ function createResource(resourceToBeCreated) {
 	return db('resources');
 }
 
-module.exports = { getAllResources, createResource };
+async function isResourceNameUnique(resource_name) {
+	const resources = await db('resources');
+	resources.forEach(resource => {
+		return resource_name === resource.resource_name ? false : true;
+	});
+}
+
+module.exports = { getAllResources, createResource, isResourceNameUnique };
