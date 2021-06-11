939eb441842ca6e20c21346b7d37d24c72aa432f
MichaelHabermas <michaelghabermas@gmail.com>
2021-06-11 11:42:09 -0400

    GET tasks done

--- api/task/model.js
+++ api/task/model.js
@@ -1,7 +1,17 @@
 const db = require('../../data/dbConfig');
 
 function getAllTasks() {
-	return db('tasks');
+	return db('tasks as t')
+		.join('projects as p', 't.project_id', 'p.project_id')
+		.select(
+			'task_id',
+			'task_description',
+			'task_notes',
+			'task_completed',
+			'project_name',
+			'project_description'
+		)
+		.orderBy('task_id');
 }
 
 function validateTask(taskToBeCreated) {
